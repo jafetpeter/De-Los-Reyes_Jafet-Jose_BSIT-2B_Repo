658f08ffcff36009759e1d1392795a995bb70775
jafetpeter <137699113+jafetpeter@users.noreply.github.com>
2025-03-19 20:57:17 -0700

    Add files via upload

---
+++ Getting Functional/function.js
@@ -0,0 +1,36 @@
+var dogName = 'rover';
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log (dogName + 'says woof woof');
+} else {
+    console.log (dogName + 'says woof woof');
+}
+
+var dogName = 'blacky';
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log (dogName + 'says woof woof');
+} else {
+    console.log (dogName + 'says woof woof');
+}
+
+var dogName = 'browny';
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log (dogName + 'says woof woof');
+} else {
+    console.log (dogName + 'says woof woof');
+}
+
+var dogName = 'whitey';
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log (dogName + 'says woof woof');
+} else {
+    console.log (dogName + 'says woof woof');
+}
+
+bark("rover", 23);
+bark("blacky", 13);
+bark("browny", 17);
+bark("whitey", 30);
